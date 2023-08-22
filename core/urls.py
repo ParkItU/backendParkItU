9ead0dd46cc56fe5c740931bc5fce6f3c6958be0
dudubarbaro <dudubarbaro>
2023-08-22 16:10:08 -0300

    feat: autenticação comentada

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+from uploader.router import router as uploader_router
+
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -37,7 +42,11 @@ router.register(r"carsingarage", CarsInGarageViewSet)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    # path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    # path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/", include(usuario_router.urls)),
+    path("api/media/", include(uploader_router.urls)),
 ]
+
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
+
