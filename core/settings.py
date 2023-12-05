dcc16c755d9ce1e77e73b34f6406707f974f972d
dudubarbaro <eduardobarbaro.ifc@gmail.com>
2023-12-05 10:46:11 -0300

    commit semi-final

--- core/settings.py
+++ core/settings.py
@@ -136,9 +136,15 @@ FILE_UPLOAD_PERMISSIONS = 0o640
 
 STATIC_URL = "/static/"
 
-if MODE == "PRODUCTION":
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+if MODE in ["PRODUCTION", "MIGRATE"]:
+    CLOUDINARY_URL = os.getenv("CLOUDINARY_URL")
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    MEDIA_URL = '/media/'
+else:
+    MY_IP = os.getenv("MY_IP", "127.0.0.1")
+    MEDIA_URL = f"http://{MY_IP}:19003/media/"
 
 if MODE in ["PRODUCTION", "MIGRATE"]:
     DATABASES = {
